632213c02f7194b3b053c4ede0856136c322d535
Esi-Akotia <esi.akotia@gmail.com>
2024-02-24 17:13:13 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,3 +6,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
+// Load the GeoJSON data
+fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+    .then(response => response.json())
+    .then(data => {
+        // Loop through each earthquake feature
+        data.features.forEach(feature => {
+            // Extract necessary properties
+            let mag = feature.properties.mag;
+            let depth = feature.geometry.coordinates[2];
+            let lng = feature.geometry.coordinates[0];
+            let lat = feature.geometry.coordinates[1];
+            let place = feature.properties.place;
+        })});      
+    console.log(mag, depth, lng, lat, place)
